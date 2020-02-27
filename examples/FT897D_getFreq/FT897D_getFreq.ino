0749998760807c3cecb6a9989f691f54ccc2d47e
Victor <ua6em@yandex.ru>
2020-02-27 20:19:18 +0300

    Fix include libs

--- examples/FT897D_getFreq/FT897D_getFreq.ino
+++ examples/FT897D_getFreq/FT897D_getFreq.ino
@@ -1,38 +1,27 @@
 /*
-  Created:  2012.08.16
-   Author:  James Buck, VE3BUX
-      Web:  http://www.ve3bux.com
-
-  If you use this code and / or the associated library, all I ask is that you "give a nod" by making a small mention
-  in your code's documentation section. I've worked hard on getting this working, so a little recognition is appreciated. :)
   
   ------------------
 
-  FT857D_controls_test
-
-  This example sketch demonstrates how to use the FT-857D library to control a Yaesu FT-857D radio by calling various commands.
-  Attach your radio's CAT interface to the following arduino pins to begin:
+  FT897D_getFreq
 
     Radio CAT port GND -> Arduino GND
-    Radio CAT port TX  -> Arduino pin 3
-    Radio CAT port RX  -> Arduino pin 2
+    Radio CAT port TX  -> Arduino pin 2
+    Radio CAT port RX  -> Arduino pin 3
 
-  If once you compile and upload the code, you are unable to see the lock icon on the radio's LCD display, try reversing
-  pins 2 and 3. It may be necessary to power down the radio and to reset the Arduino prior to powering the radio back on.
-
+ 
 */
 
 #include <SoftwareSerial.h>
-#include "FT857D.h"     // the file FT857D.h has a lot of documentation which I've added to make using the library easier
+#include "FT897D.h"     // the file FT897D.h has a lot of documentation which I've added to make using the library easier
 
-FT857D radio;           // define "radio" so that we may pass CAT commands
+FT897D radio;           // define "radio" so that we may pass CAT commands
                   
 void setup() {
 
 /*---- Working on a way to allow easier user input of I/O pins for software serial emulation
-  ---- Right now, it is necessary to edit the FT857D.cpp file and to edit the line which states:
+  ---- Right now, it is necessary to edit the FT897D.cpp file and to edit the line which states:
   ---- extern SoftwareSerial rigCat(3,2); // rx,tx
-  ---- change the rx & tx pins to values which suit your application and save the FT857D.cpp file
+  ---- change the rx & tx pins to values which suit your application and save the FT897D.cpp file
 */
 
 //  SoftwareSerial userSerial(3,2);  
